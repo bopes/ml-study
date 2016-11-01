8b275511554cb644d6bd3fdfa937fef52b444cf0
bopes <clcblee@gmail.com>
2016-11-01 12:47:38 -0700

    Train and predict

--- sklearn/intro.py
+++ sklearn/intro.py
@@ -1,11 +1,17 @@
-from sklearn import datasets
+from sklearn import datasets, svm
 
+# Load data
 iris = datasets.load_iris()
 digits = datasets.load_digits()
 
-print("Data")
-print(digits.data)
-print("Target")
-print(digits.target)
-print("original sample")
-print(digits.images[0])
+# Create Classifier
+clf = svm.SVC(gamma=0.001, C=100.)
+
+# Fit classifier to data (all but last example)
+clf.fit(digits.data[:-1], digits.target[:-1])
+
+# Predict the last example from data
+pred = clf.predict(digits.data[-1:])
+
+# Display prediction
+print(pred)
