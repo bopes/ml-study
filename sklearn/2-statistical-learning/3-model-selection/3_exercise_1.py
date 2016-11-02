35344f61e5b5d969ba803f316a8722f3db61faee
bopes <clcblee@gmail.com>
2016-11-02 13:05:10 -0700

    Exercise 1

---
+++ sklearn/2-statistical-learning/3-model-selection/3_exercise_1.py
@@ -0,0 +1,66 @@
+# Exercise 1
+# On the digits dataset, plot the cross-validation score of a SVC estimator with an linear kernel as a function of parameter C (use a logarithmic grid of points, from 1 to 10).
+
+# My Attempt
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn import datasets, svm
+
+digits = datasets.load_digits()
+x = digits.data
+y = digits.target
+
+svc = svm.SVC(kernel='linear')
+C_s = np.logspace(-10, 0, 10)
+
+scores = list()
+for C in C_s:
+  svc.C = C
+  this_score = cross_val_score(svc, x, y, n_jobs=1)
+  scores.append(np.mean(this_score))
+
+import matplotlib.pyplot as plt
+plt.figure(1, figsize=(4,3))
+plt.clf()
+plt.semilogx(C_s, scores)
+locs, labels = plt.yticks()
+plt.yticks(locs, list(map(lambda x: "%g" % x, locs)))
+plt.ylabel('CV score')
+plt.xlabel('Parameter C')
+plt.ylim(0, 1.1)
+plt.show()
+
+# Solution
+
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn import datasets, svm
+
+digits = datasets.load_digits()
+X = digits.data
+y = digits.target
+
+svc = svm.SVC(kernel='linear')
+C_s = np.logspace(-10, 0, 10)
+
+scores = list()
+scores_std = list()
+for C in C_s:
+    svc.C = C
+    this_scores = cross_val_score(svc, X, y, n_jobs=1)
+    scores.append(np.mean(this_scores))
+    scores_std.append(np.std(this_scores))
+
+# Do the plotting
+import matplotlib.pyplot as plt
+plt.figure(1, figsize=(4, 3))
+plt.clf()
+plt.semilogx(C_s, scores)
+plt.semilogx(C_s, np.array(scores) + np.array(scores_std), 'b--')
+plt.semilogx(C_s, np.array(scores) - np.array(scores_std), 'b--')
+locs, labels = plt.yticks()
+plt.yticks(locs, list(map(lambda x: "%g" % x, locs)))
+plt.ylabel('CV score')
+plt.xlabel('Parameter C')
+plt.ylim(0, 1.1)
+plt.show()
