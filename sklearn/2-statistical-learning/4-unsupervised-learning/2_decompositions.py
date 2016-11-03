736f37286c210289db3693b283ea7f528d753491
bopes <clcblee@gmail.com>
2016-11-02 17:45:42 -0700

    PCA

--- sklearn/2-statistical-learning/4-unsupervised-learning/2_decompositions.py
+++ sklearn/2-statistical-learning/4-unsupervised-learning/2_decompositions.py
@@ -19,3 +19,21 @@ pca.n_components = 2 # this sets the number of considered components
 X_reduced = pca.fit_transform(X) # this transformed the data to meet the new number of components
 X_reduced.shape # equals (100, 2) because there are 100 observatinos with 2 features
 
+# Independent Component Analysis
+# ICA selects components such that the distribution of their loadings carries the maximuum amount of independent info. It is able to reocver non-Gaussian independent signals
+
+# Generate sample data
+time = np.linspace(0,10,2000)
+s1 = np.sin(2 * time) # Signal 1 - sinusoidal signal
+s2 = np.sign(np.sin(3 * time)) # Signal 2 - square signal
+S = NP.C_[s1,s2]
+S += 0.2 * np.random.normal(size=S.shape) # Add noise
+S /= S.std(axis=0) # Standardize data
+# Mix data
+A = np.array([1,1],[0.5,2]) # Mixing matrix
+X = np.dots(S,A.T) # Generate observations
+# Compute ICA
+ica = decomposition.FastICA()
+S_ = ica.fit_transform(X) # get the estimated sources
+A_ = ica.mixing_.T
+print(np.allclose(X,np.dots(S_,A_) + ica.mean_))
