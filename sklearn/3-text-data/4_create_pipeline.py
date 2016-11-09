0414344c9940fdb51242f1798e4aef33e1c196c4
bopes <clcblee@gmail.com>
2016-11-08 16:28:00 -0800

    Create pipeline

---
+++ sklearn/3-text-data/4_create_pipeline.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+
+categories = ['alt.atheism','soc.religion.christian','comp.graphics','sci.med']
+twenty_train = fetch_20newsgroups(subset='train',categories=categories,shuffle=True,random_state=42)
+
+# Create a pipeline to make the classifier easier to work with
+from sklearn.pipeline import Pipeline
+text_clf = Pipeline([
+  ('vect',  CountVectorizer()  ),
+  ('tfidf', TfidfTransformer() ),
+  ('clf',   MultinomialNB()    ),
+])
